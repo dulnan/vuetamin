c56914f800d43b8970a27e9e6505cc1a7836d87a
Jan Hug <me@dulnan.net>
2018-10-27 10:52:59 +0200

    Comments

--- src/classes/Store.js
+++ src/classes/Store.js
@@ -22,8 +22,18 @@ export default class Store {
   }
 
   /**
+   * Returns the state from data by calling the user-defined
+   * state function.
+   * 
+   * @returns {Any}
+   */
+  getState () {
+    return this._getState(this.data)
+  }
+
+  /**
    * Loop over the properties of an object, validate them and
-   * build a new object.
+   * build a new object. Used for adding actions and mutations.
    *
    * @param {Object} properties 
    */
@@ -37,15 +47,25 @@ export default class Store {
     return entries
   }
 
+  /**
+   * Calls the mutation with the given payload and passes
+   * the context to it.
+   *
+   * @param {String} name Name of the mutation
+   * @param {Any} value Payload
+   */
   mutate (name, value) {
     this.mutations[name].call(this, this._context, value)
   }
 
+  /**
+   * Calls the action with the given payload and passes
+   * the context to it.
+   *
+   * @param {String} name Name of the action
+   * @param {Any} value Payload
+   */
   action (name, value) {
     this.actions[name].call(this, this._context, value)
-  }
-
-  getState () {
-    return this._getState(this.data)
   }
 }
